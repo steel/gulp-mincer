eb8538b52bfe7f6fa099c20c8cb423f4b1d26fbd
Steel Fu <steelfu@gmail.com>
2015-05-29 17:32:29 -0700

    wip sourcemap output

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var through     = require('through2');
-var PluginError = require('gulp-util').PluginError;
+// var PluginError = require('gulp-util').PluginError;
+var gutil = require('gulp-util');
+var gfile = require('gulp-file');
+var path = require('path');
 
 // consts
 const PLUGIN_NAME = 'gulp-mincer';
@@ -13,17 +16,27 @@ module.exports = function(env) {
 
     var asset = env.findAsset(file.path);
 
+    var cleansedFilename = path.basename(asset.pathname.replace(".coffee", ""));
+
+    var sourceMapName = cleansedFilename + ".map";
+
+    var sourceMappingUrlComment = "\n//# sourceMappingUrl=" + sourceMapName;
+
     if ( file.isStream() ) {
       var stream = through();
 
-      stream.write(asset.toString());
+      stream.write(asset.toString() + sourceMappingUrlComment);
 
       file.contents = stream;
     }
 
     if ( file.isBuffer() ) {
-      file.contents = new Buffer(asset.toString())
+      file.base = "/";
+      file.path = "/" + cleansedFilename;
+      file.contents = new Buffer(asset.toString() + sourceMappingUrlComment);
     }
+
+    this.emit("sourceMap", gfile(sourceMapName, asset.sourceMap, {src: true}));
 
     cb(null, file);
   });
